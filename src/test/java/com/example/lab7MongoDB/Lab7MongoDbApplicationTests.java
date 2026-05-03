8efdf3447e214e753a5e0a6d89d65da0c4684f0b
Fab1annnnnn <fabiandresbaron@gmail.com>
2026-05-03 16:08:54 -0500

    feat: proyecto base Spring Boot

---
+++ src/test/java/com/example/lab7MongoDB/Lab7MongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab7MongoDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab7MongoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
